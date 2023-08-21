d6242233614fe6b86f98b72d39936da122753536
Stefan Cvetanovski <stefan.cvetanovski@polarcape.com>
2023-08-21 19:17:27 +0200

    Add function for adding project

--- src/components/CreateProjectForm/CreateProjectForm.jsx
+++ src/components/CreateProjectForm/CreateProjectForm.jsx
@@ -2,14 +2,22 @@ import React, { useState } from "react";
 import { InputField } from "../shared/InputField";
 import { Button } from "../shared/Button";
 import { useNavigate } from "react-router-dom";
-import styles from "./CreateProjectForm.module.scss";
 import { SelectField } from "../shared/SelectField";
+import { useProjects } from "../../context/ProjectsContext";
+import styles from "./CreateProjectForm.module.scss";
 
 const CreateProjectForm = ({ services }) => {
+  const { addProject } = useProjects();
   const [credentials, setCredentials] = useState({ name: "", service: null });
   const navigate = useNavigate();
   const handleSubmit = (e) => {
     e.preventDefault();
+    const body = {
+      ...credentials,
+      service_id: credentials?.service?.value || "",
+    };
+    delete body.service;
+    addProject(body);
   };
 
   const handleChangeInput = (e) => {
--- src/context/ProjectsContext.js
+++ src/context/ProjectsContext.js
@@ -1,12 +1,15 @@
 import React, { createContext, useContext, useState } from "react";
-import { PROJECTS_ENDPOINT } from "../api/endpoints";
+import { PROJECTS_ENDPOINT, SERVICES_ENDPOINT } from "../api/endpoints";
 import { useApiClient } from "../hooks/useApiClient";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-hot-toast";
 
 const ProjectsContext = createContext();
 
 export const ProjectsProvider = ({ children }) => {
   const [projects, setProjects] = useState([]);
   const api = useApiClient();
+  const navigate = useNavigate();
 
   const getProjects = async () => {
     try {
@@ -15,11 +18,20 @@ export const ProjectsProvider = ({ children }) => {
     } catch (error) {}
   };
 
+  const addProject = async (payload) => {
+    try {
+      await api.post(SERVICES_ENDPOINT, payload);
+      toast.success("Project Created!");
+      navigate("/");
+    } catch (error) {}
+  };
+
   return (
     <ProjectsContext.Provider
       value={{
         projects,
         getProjects,
+        addProject,
       }}
     >
       {children}
